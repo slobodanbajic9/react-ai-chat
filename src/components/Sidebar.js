000ae73f702c622005fb3df3455d5f9239b8a60c
slobodanbajic9 <slobodan.bajic9@gmail.com>
2024-11-09 23:20:23 +0100

    Optimize sidebar for mobile device

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,48 +1,40 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import DeleteModal from "./DeleteModal";
 import { MdDeleteOutline } from "react-icons/md";
 import { CiCirclePlus } from "react-icons/ci";
 
-function Sidebar({ onSelectConversation, onNewChat }) {
-  const [conversations, setConversations] = useState([]);
-  const [isDeleteModalOpen, setIsDeletedModalOpen] = useState(false);
+function Sidebar({
+  conversations,
+  onSelectConversation,
+  onNewChat,
+  isOpen,
+  onClose,
+  handleDeleteConversation,
+}) {
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
   const [selectedConversationId, setSelectedConversationId] = useState(null);
-
-  useEffect(() => {
-    const fetchConversations = async () => {
-      try {
-        const response = await axios.get(
-          "http://localhost:8000/api/conversations"
-        );
-        setConversations(response.data);
-      } catch (error) {
-        console.error("Error fetching conversations:", error);
-      }
-    };
-
-    fetchConversations();
-  }, []);
+  const maxTitleLength = 20;
 
   const openDeleteModal = (id) => {
     setSelectedConversationId(id);
-    setIsDeletedModalOpen(true);
+    setIsDeleteModalOpen(true);
   };
 
   const closeDeleteModal = () => {
-    setIsDeletedModalOpen(false);
+    setIsDeleteModalOpen(false);
     setSelectedConversationId(null);
   };
 
-  const handleDeleteConversation = async () => {
+  const handleDeleteChat = async () => {
     try {
       await axios.delete(
         `http://localhost:8000/api/conversations/${selectedConversationId}`
       );
-      setConversations((prevConversations) =>
-        prevConversations.filter((conv) => conv._id !== selectedConversationId)
-      );
 
+      handleDeleteConversation(selectedConversationId);
+
+      onClose();
       closeDeleteModal();
     } catch (error) {
       console.log("Error deleting conversation", error);
@@ -50,41 +42,58 @@ function Sidebar({ onSelectConversation, onNewChat }) {
   };
 
   return (
-    <div className="fixed sidebar bg-gray-800 text-white w-64 p-2 h-full">
-      <div className="relative h-full">
+    <div
+      className={`fixed top-0 left-0 h-full bg-gray-800 text-white w-64 p-4 transform transition-transform duration-300 ${
+        isOpen ? "translate-x-0" : "-translate-x-full"
+      } sm:translate-x-0 z-20`}>
+      <div className="relative h-full flex flex-col">
         <h2 className="text-xl mb-4 p-2">Recent Conversations</h2>
-        <ul>
+
+        <ul className="flex-grow overflow-y-auto">
           {conversations.map((conversation) => (
             <li
               key={conversation._id}
-              className="flex justify-between items-center cursor-pointer p-2 hover:bg-gray-700 rounded pb-4">
+              className="flex justify-between items-center cursor-pointer p-2 hover:bg-gray-700 rounded">
               <span
-                onClick={() =>
-                  onSelectConversation(conversation.history, conversation._id)
-                }>
-                {conversation.history[0]?.content}
+                onClick={() => {
+                  onSelectConversation(conversation.history, conversation._id);
+                  onClose();
+                }}>
+                {conversation.history[0]?.content.length > maxTitleLength
+                  ? conversation.history[0].content.slice(0, maxTitleLength) +
+                    "..."
+                  : conversation.history[0].content}
               </span>
               <button
-                onClick={() => openDeleteModal(conversation._id)}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  openDeleteModal(conversation._id);
+                }}
                 className="ml-4 text-red-500">
                 <MdDeleteOutline size={25} />
               </button>
             </li>
           ))}
         </ul>
+
         {/* New chat button */}
-        <div className="absolute flex text-center items-center gap-2 pointer bottom-4 left-4 mt-4 p-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-          <button onClick={onNewChat} className="text-white">
-            New chat
+        <div className="mt-4">
+          <button
+            onClick={() => {
+              onNewChat();
+              onClose();
+            }}
+            className="flex items-center gap-2 p-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+            <CiCirclePlus size={20} />
+            <span>New chat</span>
           </button>
-          <CiCirclePlus size={25} />
         </div>
 
         {/* Delete Modal */}
         <DeleteModal
           isOpen={isDeleteModalOpen}
           onClose={closeDeleteModal}
-          onConfirm={handleDeleteConversation}
+          onConfirm={handleDeleteChat}
         />
       </div>
     </div>
